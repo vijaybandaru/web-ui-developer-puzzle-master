718a84db3f18133d6679af95601323520a7077e4
Vijay Bandaru <vbandaru@mccorp.com>
2023-07-08 07:53:31 +0530

    Test cases for finished book status

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -11,4 +11,14 @@ describe('When: I use the reading list feature', () => {
       'My Reading List'
     );
   });
+
+  it('Then: Update finished status to a book', () => {
+    cy.get('input[type="search"]').type('javascript');
+    cy.get('form').submit();
+    cy.get("#btn-nBuA0hmspdMC").click();
+    cy.get('[data-testing="toggle-reading-list"]').click();
+    cy.get("#finishedbtn-nBuA0hmspdMC").click();
+    cy.get("#finishedbtn-nBuA0hmspdMC").should('contain.text', 'Finished');
+  });
+
 });
